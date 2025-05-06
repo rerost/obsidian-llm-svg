22c4f8ce0302fcba70a21cca0ea06c824c37836d
Hazumi Ichijo <hahihu314+github@gmail.com>
2025-05-06 23:52:57 +0900

    Parse image/svg

--- src/Plugin.ts
+++ src/Plugin.ts
@@ -24,22 +24,23 @@ export class Plugin extends PluginBase<PluginTypes> {
 
   protected override async onloadImpl(): Promise<void> {
     await super.onloadImpl();
-    this.registerMarkdownCodeBlockProcessor('llm-svg', this.handleSampleCodeBlockProcessor.bind(this));
+    this.registerMarkdownCodeBlockProcessor('llm-svg', this.handleCodeBlockProcessor.bind(this));
   }
 
-  private async handleSampleCodeBlockProcessor(source: string, el: HTMLElement, _: MarkdownPostProcessorContext): Promise<void> {
+  private async handleCodeBlockProcessor(source: string, el: HTMLElement, _: MarkdownPostProcessorContext): Promise<void> {
     const sourceHash = hash(source)
-    const loadingDiv = document.createElement('div')
-    loadingDiv.setAttribute("class", "llm-svg-loading")
-    loadingDiv.innerHTML = "SVG Generating..."
-    el.appendChild(loadingDiv)
+    const target = el.createEl('div')
+    const loadingDiv = target.createEl('div', { text: "SVG Generating...", cls: "llm-svg-loading" })
 
     const generator = new SVGGenerator(this.settings.apiKey)
     const svgCode = await generator.generateSVG(this.settings.model, source)
 
-    const div = document.createElement('div')
-    div.setAttribute("class", "llm-svg-" + sourceHash)
-    div.innerHTML = svgCode
+    const parser = new DOMParser();
+    const svgDoc = parser.parseFromString(svgCode, "image/svg+xml");
+    const svgElement = svgDoc.documentElement;
+
+    const div = target.createEl('div', { cls: "llm-svg-" + sourceHash })
+    div.appendChild(svgElement);
 
     el.appendChild(div);
     el.removeChild(loadingDiv)
